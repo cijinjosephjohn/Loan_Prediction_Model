704fc968ee0771db596dd82bd6b6a6d11da0057e
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2022-12-26 13:35:23 +0530

    modified explainer.py

--- explainer.py
+++ explainer.py
@@ -7,18 +7,18 @@ x_train = pickle.load(open("x_train","rb"))
 x_test = pickle.load(open("x_test","rb"))
 model = load_model('models/model.h5')
 
-scale = StandardScaler()
-x_train = scale.fit_transform(x_train)
-x_test = scale.fit_transform(x_test)
+# scale = StandardScaler()
+# x_train = scale.fit_transform(x_train)
+# x_test = scale.fit_transform(x_test)
 # explainer = shap.DeepExplainer(model,x_train)
 # shap_values = explainer.shap_values(x_train)
 
-
-
 shap.initjs()
+explainer_shap = shap.DeepExplainer(model=model,data = x_train)
 
-print("ans")
-print(x_train)
+
+# print("ans")
+# print(x_train)
 # shap.summary_plot(shap_values[0],plot_type="bar",feature_names=x_test.columns)
 
 # shap.plots.bar(shap_values)
--- model.py
+++ model.py
@@ -6,6 +6,7 @@ x_train = pandas.read_pickle("x_train")
 y_train = pandas.read_pickle("y_train")
 
 
+
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(units=4,activation="relu"))
 model.add(tf.keras.layers.Dense(units=4,activation="relu"))
@@ -17,3 +18,4 @@ model.compile(optimizer="adam",loss="binary_crossentropy",metrics=['accuracy'])
 model.fit(x_train,y_train,epochs=13)
 import os 
 model.save(os.path.join("models","model.h5"))
+
